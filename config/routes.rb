5f709abc17222b5fd49dd2e7c04651b003e108bd
EC2 Default User <ec2-user@ip-172-31-12-168.ap-northeast-1.compute.internal>
2023-04-09 02:41:20 +0000

    間違えて作ったnewのファイルを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'index' => 'books#index'
+  get 'books/show'
+  get 'books/edit'
+  get 'top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
